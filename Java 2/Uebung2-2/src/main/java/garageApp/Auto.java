fc321ab96300ea83c6e31efad0f0c46bfea7c86c
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2023-09-05 20:03:23 +0200

    A

---
+++ Java 2/Uebung2-2/src/main/java/garageApp/Auto.java
@@ -0,0 +1,12 @@
+package garageApp;
+
+public class Auto extends Fahrzeug{
+    public Auto(){
+        super();
+        setAnzahlRaeder(4);
+    }
+
+    public String toString(){
+        return "Typ: Auto\n" + super.toString();
+    }
+}
