a66837c4031f1e37910723363de99fca83036864
Luca Bützberger <luca.buetzberger@gmail.com>
2023-12-16 13:09:10 +0100

    -

---
+++ BBIN22.2a.IN213 Algorithmen und Datenstrukturen/Datenpufferung/src/main/java/ch/hftm/StackClient.java
@@ -0,0 +1,14 @@
+package ch.hftm;
+
+public class StackClient {
+    
+    public static void main(String[] args){
+        ArrayStack stack = new ArrayStack();
+        System.out.println(stack.pop());
+        stack.push(5);
+        stack.push(10);
+        stack.push(50);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
