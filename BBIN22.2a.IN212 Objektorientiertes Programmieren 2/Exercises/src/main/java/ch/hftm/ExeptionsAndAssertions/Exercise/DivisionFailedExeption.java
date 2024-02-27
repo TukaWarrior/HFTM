f14f6091fae29f401b90c2a21305a7635eed41f9
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-02-27 11:49:39 +0100

    -

--- BBIN22.2a.IN212 Objektorientiertes Programmieren 2/Exercises/src/main/java/ch/hftm/ExeptionsAndAssertions/Exercise/DivisionFailedExeption.java
+++ BBIN22.2a.IN212 Objektorientiertes Programmieren 2/Exercises/src/main/java/ch/hftm/ExeptionsAndAssertions/Exercise/DivisionFailedExeption.java
@@ -7,10 +7,12 @@ public class DivisionFailedExeption extends Exception {
     private String operand2;
 
     public DivisionFailedExeption(String exception) {
+        super();
         this.exception = exception;
     }
 
     public DivisionFailedExeption(String exception, String operand1, String operand2) {
+        super();
         this.exception = exception;
         this.operand1 = operand1;
         this.operand2 = operand2;
