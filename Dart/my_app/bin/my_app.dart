8fddcf8ac46f9500fa5b12d4742cd01f435e532b
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-05-17 12:36:03 +0200

    -

--- Dart/my_app/bin/my_app.dart
+++ Dart/my_app/bin/my_app.dart
@@ -1,26 +1,22 @@
-import 'dart:convert';
-import 'dart:io';
-
-import 'package:my_app/car.dart';
-import 'package:my_app/movie.dart';
-import 'package:my_app/my_app.dart' as my_app;
+import 'package:my_app/app.dart' as app;
 
 void main(List<String> arguments) {
-  // print('Hello world: ${my_app.calculate()}!');
-
-  var _movies = [];
-  print('Enter a movie name:');
-  var line_name = stdin.readLineSync(encoding: utf8);
-  print('Enter the producer:');
-  var line_creator = stdin.readLineSync(encoding: utf8);
-  print('Enter a release year:');
-  var line_year = stdin.readLineSync(encoding: utf8);
-  print('Enter a length:');
-  var line_length = stdin.readLineSync(encoding: utf8);
-
-  var movie = Movie(line_name.toString(), line_creator.toString(),
-      line_year.toString(), line_length.toString());
-  _movies.add(movie);
-
-  print(_movies);
+  app.main(arguments);
 }
+
+
+//   var _movies = [];
+//   print('Enter a movie name:');
+//   var line_name = stdin.readLineSync(encoding: utf8);
+//   print('Enter the producer:');
+//   var line_creator = stdin.readLineSync(encoding: utf8);
+//   print('Enter a release year:');
+//   var line_year = stdin.readLineSync(encoding: utf8);
+//   print('Enter a length:');
+//   var line_length = stdin.readLineSync(encoding: utf8);
+
+//   var movie = Movie(line_name.toString(), line_creator.toString(),
+//       line_year.toString(), line_length.toString());
+//   _movies.add(movie);
+
+//   print(_movies);
